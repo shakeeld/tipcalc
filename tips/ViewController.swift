136c19e8f5ebf6fc83c87d649fb4b26b58cdc984
Shakeel Daswani <shakeeldaswani@Shakeels-MacBook-Pro.local>
2016-01-01 03:34:57 +0900

    Added a launch screen and fixed the app icons.

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -9,15 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        self.title = "Tip Calculator"
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func onEditingChanged(sender: AnyObject) {
+    var tipPercentages = [0.18, 0.2, 0.22]
+    var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+    let billAmount = NSString(string: billField.text!).doubleValue
+    let tip = billAmount * tipPercentage
+    let total = tip + billAmount
+    
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
     }
 
 
